3ee098d1760d0af8c83abfd548ae7edc3bd886ec
scanhand <scanhand@gmail.com>
2019-10-08 21:13:42 +0900

    changed to PSOC Breathing LED

--- PSOCLEDBlink/PSOCLEDBlink.cydsn/main.c
+++ PSOCLEDBlink/PSOCLEDBlink.cydsn/main.c
@@ -36,13 +36,9 @@
 *******************************************************************************/
 int main()
 {
-	/* Start LCD and PWM */
-	LCD_Char_Start();
+	/* Start PWM */
 	PWM_Start();
 	
-	/* Print Hello World on LCD */
-	LCD_Char_PrintString("Hello World");
-
     /* Loop forever */
 	for(;;)
     {
